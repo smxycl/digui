144d357ed52ae0796036a863dca84099f1d18688
smxycl <80041652+smxycl@users.noreply.github.com>
2021-03-19 18:14:27 +0800

    Add files via upload

---
+++ 全排列问题.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+int fact(int n);
+int per(int n, int m);//per表示全排列
+int main()
+{
+	int n, m;
+	printf("元素n=");
+	scanf_s("%d", &n);
+	printf("取出元素m=%d");
+	scanf_s("%d", &m);
+	printf("%d\n", per(n, m));
+}
+int fact(int n)
+{
+	if (n < 0)
+		return 0;
+	else if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * fact(n - 1);
+}
+int per(int n,int m)
+{
+	int A;
+	A = fact(n) / fact(n - m);
+	return A;
+}
